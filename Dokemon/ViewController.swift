7a79866f3101ea8cbb56c39d7a3aad43fd95e36d
edo Lorenza <edolrnz@gmail.com>
2021-06-20 12:00:02 +0700

    Initial Commit

---
+++ Dokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dokemon
+//
+//  Created by Edo Lorenza on 20/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
