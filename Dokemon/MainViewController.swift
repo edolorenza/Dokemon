5452a25eeb22b7f1d006a3d4ba6f702b7bd11782
edo Lorenza <edolrnz@gmail.com>
2021-06-20 13:08:08 +0700

    setup view controller

---
+++ Dokemon/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Dokemon
+//
+//  Created by Edo Lorenza on 20/06/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Dokemon"
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
